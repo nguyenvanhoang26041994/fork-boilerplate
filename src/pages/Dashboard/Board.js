3111af06889e33eb28331cb1fe4ff52994998ad7
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-04-04 10:44:03 +0700

    Fix

--- src/pages/Dashboard/Board.js
+++ src/pages/Dashboard/Board.js
@@ -2,14 +2,11 @@ import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 
-import { Overview } from '../../components';
 
 const Board = ({ className }) => {
   return (
     <div className="board">
-      <Overview>
-        
-      </Overview>
+
     </div>
   )
 };
