a71b1709071399b63b70a8d61edbda38f6925f4b
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-04-12 16:53:33 +0700

    Fix tool

--- tools/make-icons.js
+++ tools/make-icons.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const make = function(text) {
+const makeTablerIcons = function() {
+  fs.readdir(path.resolve('tabler-icons/icons'), function(err, files) {
+    const _files = files.map(file => file.replace('.svg', ''))
+    const content = _files.map(file => `  '${file}': require('./${file}').default,`).join('\n');
+    const text = `const icons = {\n${content}\n};\n\nexport default icons;\n`;
+    fs.writeFileSync(path.resolve('rc-neumorphism/src/components/Icon/svg/index.js'), text);
   
+    _files.map(file => {
+      fs.readFile(path.resolve(`tabler-icons/icons/${file}.svg`), function(err, linkString) {
+        let _linkString = String(linkString).replace(/\<svg .+\>/, '<svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">');
+        _linkString = _linkString.replace(/\n+$/, '');
+        _linkString = _linkString.replace('stroke-dasharray', 'strokeDasharray');
+        _linkString = _linkString.replace('stroke-dashoffset', 'strokeDashoffset');
+        const component = `import React from 'react';\n\nexport default props => (\n${_linkString}\n);\n`;
+        fs.writeFileSync(path.resolve(`rc-neumorphism/src/components/Icon/svg/${file}.js`), component);
+      })
+    });
+  });
 };
 
-fs.readdir(path.resolve('tabler-icons/icons'), function(err, files) {
-  const _files = files.map(file => file.replace('.svg', ''))
-  const content = _files.map(file => `  '${file}': require('./${file}').default,`).join('\n');
-  const text = `const icons = {\n${content}\n};\n\nexport default icons;\n`;
-  fs.writeFileSync(path.resolve('result-tools/Icon/svg/index.js'), text);
+const makeIcons = function() {
+  makeTablerIcons();
+};
 
-  _files.map(file => {
-    fs.readFile(path.resolve(`tabler-icons/icons/${file}.svg`), function(err, linkString) {
-      let _linkString = String(linkString).replace(/\<svg .+\>/, '<svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">');
-      _linkString = _linkString.replace(/\n+$/, '');
-      _linkString = _linkString.replace('stroke-dasharray', 'strokeDasharray');
-      _linkString = _linkString.replace('stroke-dashoffset', 'strokeDashoffset');
-      const component = `import React from 'react';\n\nexport default props => (\n${_linkString}\n);\n`;
-      fs.writeFileSync(path.resolve(`result-tools/Icon/svg/${file}.js`), component);
-    })
-  });
-});
+makeIcons();
