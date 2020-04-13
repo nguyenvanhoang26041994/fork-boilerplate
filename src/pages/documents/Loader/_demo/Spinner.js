5a4715a3fac7bcbca51242be0dbb0e15aa96bd32
Son Le <sontdt69@gmail.com>
2020-04-13 15:44:42 +0700

    Refactor document

--- src/pages/documents/Loader/_demo/Spinner.js
+++ src/pages/documents/Loader/_demo/Spinner.js
@@ -9,14 +9,14 @@ const Demo = () => {
         height: '100px',
       }}
     >
-      <div className="flex-1" style={{ marginBottom: '10px' }}>
+      <div className="flex-1">
         <Loader.Spinner color="red" />
       </div>
-      <div className="flex-1" style={{ marginBottom: '10px' }}>
+      <div className="flex-1">
         <Loader.Spinner />
       </div>
-      <div className="flex-1" style={{ marginBottom: '10px' }}>
-        <Loader.Spinner size="5em" />
+      <div className="flex-1">
+        <Loader.Spinner size="3em" />
       </div>
     </div>
   );
@@ -37,14 +37,14 @@ const Demo = () => {
         height: '100px',
       }}
     >
-      <div className="flex-1" style={{ marginBottom: '10px' }}>
+      <div className="flex-1">
         <Loader.Spinner color="red" />
       </div>
-      <div className="flex-1" style={{ marginBottom: '10px' }}>
+      <div className="flex-1">
         <Loader.Spinner />
       </div>
-      <div className="flex-1" style={{ marginBottom: '10px' }}>
-        <Loader.Spinner size="5em" />
+      <div className="flex-1">
+        <Loader.Spinner size="3em" />
       </div>
     </div>
   );
