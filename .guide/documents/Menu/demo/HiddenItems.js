2188ae4c3912b7cad7b46dc64e2bd3228fc8482f
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-04-18 23:25:26 +0700

    Fix

---
+++ .guide/documents/Menu/demo/HiddenItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Menu } from '@/rc-neumorphism/core';
+
+const Demo = () => {
+  return (
+    <Menu hiddenKeys={['option-2', 'option-3']}>
+      <Menu.Item key="option-1" title="Option One" />
+      <Menu.Item key="option-2" title="Option Two" />
+      <Menu.Item key="option-3" title="Option Three" />
+      <Menu.Item key="option-4" title="Option Four" />
+    </Menu>
+  );
+};
+
+export default Demo;
