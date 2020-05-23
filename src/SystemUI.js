3766f8aeb4dc8c04e79068f2d76bfca3ca8d590f
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-05-23 21:57:35 +0700

    Fix

--- src/SystemUI.js
+++ src/SystemUI.js
@@ -29,13 +29,36 @@ import {
   Chip,
   Dialog,
   SpeedDial,
+  PureNotification,
+  PureAlert,
+  Upload,
+  Divider,
+  Breadcrumb,
+  Carousel,
+  Typo,
 } from './rc-neumorphism/core';
 
-const StyledFlex = styled(Flex)`
-  > * {
-    margin-right: 1em;
-  }
+const listProduct = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const ColorWrapper = styled(Flex)`
+  min-width: 7rem;
+  min-height: 7rem;
+  display: flex;
+  flex-direction: column;
+  box-shadow: var(--overlay--shadow);
+  border-radius: var(--border-radius);
+  overflow: hidden;
 `;
+const ColorShower = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Color = ({ value }) => (
+  <ColorWrapper>
+    <ColorShower style={{ backgroundColor: value }} />
+  </ColorWrapper>
+);
 
 const SectionWrapper = styled(Flex)`
   width: 100%;
@@ -88,7 +111,7 @@ const Loren = () => (
         <Skeleton p  w="20%" />
       </Flex>
     </Flex>
-    <Skeleton rect w="100%" h="300px" />
+    <Skeleton rect w="100%" h="100px" />
   </Flex>
 );
 
@@ -139,7 +162,7 @@ const ReactTab = () => {
             <Skeleton p  w="20%" />
           </Flex>
         </Flex>
-        <Skeleton rect w="100%" h="300px" />
+        <Skeleton rect w="100%" h="100px" />
       </Flex>
     )
   }
@@ -154,33 +177,61 @@ export default () => {
   return (
     <Flex span="full" style={{ backgroundColor: 'var(--bg)' }}>
       <Flex col span="1/2">
-          <Section>
-            <Tabs>
-              <Tabs.Item key="_1" title="React" icon="send"><ReactTab /></Tabs.Item>
-              <Tabs.Item key="_4" title="Javascript" icon="thumb-up"><ReactTab /></Tabs.Item>
-              <Tabs.Item key="_5" title="Badge" icon={<Badge leftTop count="2" className="mr-4"><Icon name="messages" /></Badge>} ><ReactTab /></Tabs.Item>
-              <Tabs.Item key="_7" title="Fresh" icon="repeat" fresh ><ReactTab /></Tabs.Item>
-            </Tabs>
-          </Section>
-        <Section>
-          <Progress.Circle percent={0.75} />
-          <Flex style={{ flexGrow: 1, padding: '0 2rem' }} items="center">
-            <Flex col className="w-full">
-              <Progress.Linear percent={0.75} />
-              <Rater
-                max={7}
-                star={5}
-                fontSize="1.5rem"
-              />
+        <Section className="--space-10">
+          <Color value="var(--red--500)" />
+          <Color value="var(--orange--500)" />
+          <Color value="var(--green--500)" />
+          <Color value="var(--blue--500)" />
+          <Color value="var(--bg)" />
+          <Color value="var(--secondary-bg)" />
+        </Section>
+        <Section col style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+          <Typo h6>Heading</Typo>
+          <Typo h5>Heading</Typo>
+          <Typo h4>Heading</Typo>
+          <Typo h3>Heading</Typo>
+          <Typo h2>Heading</Typo>
+          <Typo h1>Heading</Typo>
+          <Typo p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </Typo>
+          <Typo p underline>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </Typo>
+          <Typo p through>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </Typo>
+          <Typo p italic>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </Typo>
+          <Typo p strong>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          </Typo>
+        </Section>
+        <Section col style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+          <Flex items="center">
+            <Skeleton circle size="2rem" />
+            <Flex col className="ml-2 flex-1">
+              <Skeleton p  w="70%" />
+              <Skeleton p  w="20%" />
             </Flex>
           </Flex>
+          <Skeleton rect w="100%" h="100px" />
         </Section>
         <Section style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-          <Collapse>
-            <Collapse.Item key="_1" title="REACT COLLAPSE TITLE" icon="send"><ReactTab /></Collapse.Item>
-            <Collapse.Item key="_3" title="ANGULAR COLLAPSE TITLE" icon="layers-difference" ><ReactTab /></Collapse.Item>
-            <Collapse.Item key="_4" title="BADGE COLLAPSE TITLE" icon={<Badge leftTop count="2" className="mr-4"><Icon name="messages" /></Badge>} ><ReactTab /></Collapse.Item>
-            <Collapse.Item key="_6" title="REFRESH COLLAPSE TITLE" icon="repeat" fresh><ReactTab /></Collapse.Item>
+          <Tabs>
+            <Tabs.Item key="_1" title="React" icon="send"><ReactTab /></Tabs.Item>
+            <Tabs.Item key="_4" title="Javascript" icon="thumb-up"><ReactTab /></Tabs.Item>
+            <Tabs.Item key="_5" title="Badge" icon={<Badge leftTop count="2" className="mr-4"><Icon name="messages" /></Badge>} ><ReactTab /></Tabs.Item>
+            <Tabs.Item key="_7" title="Fresh" icon="repeat" fresh ><ReactTab /></Tabs.Item>
+          </Tabs>
+        </Section>
+        <Section style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+          <Collapse defaultActiveKeys={['_4']} accordion>
+            <Collapse.Item key="_1" title="Contact" icon="send"><ReactTab /></Collapse.Item>
+            <Collapse.Item key="_3" title="Skills" icon="layers-difference" ><ReactTab /></Collapse.Item>
+            <Collapse.Item key="_4" title="Experiences" icon={<Badge leftTop count="2" className="mr-4"><Icon name="messages" /></Badge>} ><ReactTab /></Collapse.Item>
+            <Collapse.Item key="_6" title="Hobbit" icon="repeat" fresh><ReactTab /></Collapse.Item>
           </Collapse>
         </Section>
         <Section className="--space-10" style={{ paddingTop: '30px', paddingBottom: '30px' }}>
@@ -240,58 +291,50 @@ export default () => {
           </Timeline>
           </div>
         </Section>
-        <Section className="--space-10" style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-          <Slider defaultValue={80} className="w-full" />
-        </Section>
-        <Section style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-          <Dialog className="w-full">
-            <Dialog.Header>Dialog Title</Dialog.Header>
-            <Dialog.Body>
-              <Flex col style={{ padding: '0.5 1.5rem' }} className="w-full">
-                <Flex items="center">
-                  <Skeleton circle size="2rem" />
-                  <Flex col className="ml-2 flex-1">
-                    <Skeleton p  w="70%" />
-                    <Skeleton p  w="20%" />
-                  </Flex>
+        <Section className="--space-5 w-full">
+          <Carousel>
+            {listProduct.map(id => (
+              <Carousel.Item key={id} style={{ borderRadius: '0.5rem' }}>
+                <Flex col items="center" justify="center"
+                  style={{
+                    borderRadius: '1rem',
+                    height: '20rem',
+                    minWidth: '16rem',
+                  }}
+                >
+                  <ReactTab className="w-full" />
                 </Flex>
-                <Skeleton rect w="100%" h="300px" />
-              </Flex>
-            </Dialog.Body>
-            <Dialog.Footer className="flex justify-end">
-              <Button glassed><Icon name="apps" />Cancel</Button>
-              <Button glassed style={{ marginLeft: '0.5em' }}><Icon name="send" />OK</Button>
-            </Dialog.Footer>
-          </Dialog>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Section>
+        <Section col style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+          <Divider left dashed title={<Icon name="power" />} />
+          <Divider left title="LEFT TITLE" />
+          <Divider right dashed title={<Icon name="copy" />} />
+          <Divider
+            right
+            dashed
+            title={(
+              <span>
+                <Icon name="copy" className="mr-3" />
+                RIGHT TITLE
+              </span>
+            )}
+          />
         </Section>
       </Flex>
 
       <Flex col span="1/2">
         <Section className="--space-5">
-          <Button primary>Primary</Button>
-          <Button danger>Danger</Button>
-          <Button glassed>Glassed</Button>
+          <Button>Button</Button>
           <ButtonGroup>
             <Button><Icon name="send" />Send</Button>
             <Button><Icon name="repeat" />Repeat</Button>
             <Button><Icon name="cloud-download" />Download</Button>
           </ButtonGroup>
-          <ButtonGroup glassed>
-            <Button loading circle icon="send" />
-            <Button icon="repeat" />
-            <Button icon="apps" />
-          </ButtonGroup>
+          <Button loading><Icon name="send" />Send</Button>
           <Button loading circle icon="repeat" />
-        </Section>
-        <Section col style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-          <Flex items="center">
-            <Skeleton circle size="2rem" />
-            <Flex col className="ml-2 flex-1">
-              <Skeleton p  w="70%" />
-              <Skeleton p  w="20%" />
-            </Flex>
-          </Flex>
-          <Skeleton rect w="100%" h="300px" />
         </Section>
         <Section col style={{ paddingTop: '20px', paddingBottom: '20px' }}>
           <Flex className="w-full">
@@ -317,7 +360,23 @@ export default () => {
             </Stepper>
           </Flex>
         </Section>
-        <Section className="--space-10" style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+        <Section style={{ paddingTop: '30px', paddingBottom: '10px' }}>
+          <Progress.Circle percent={0.75} />
+          <Flex style={{ flexGrow: 1, padding: '0 2rem' }} items="center">
+            <Flex col className="w-full">
+              <Progress.Linear percent={0.75} />
+              <Rater
+                max={7}
+                star={5}
+                fontSize="1.5rem"
+              />
+            </Flex>
+          </Flex>
+        </Section>
+        <Section className="--space-10" style={{ paddingTop: '10px', paddingBottom: '30px' }}>
+          <Slider defaultValue={80} className="w-full" />
+        </Section>
+        <Section className="--space-10" style={{ paddingTop: '30px', paddingBottom: '10px' }}>
           <Pagination
             activePage={activePage}
             total={300}
@@ -329,7 +388,7 @@ export default () => {
           <Loader.Dots wave />
           <Loader.Spinner />
         </Section>
-        <Section className="--space-5" style={{ paddingTop: '30px', paddingBottom: '30px' }} items="center">
+        <Section className="--space-5" style={{ paddingTop: '10px', paddingBottom: '10px' }} items="center">
           <Avatar name="Hoang Nguyen" />
           <Badge
             dot
@@ -353,7 +412,7 @@ export default () => {
           </Badge>
           <Chip label="Hoàng Nguyễn" />
           <Chip label="Sơn Lê" />
-          <SpeedDial right overlay={
+          <SpeedDial right defaultVisible overlay={
             <React.Fragment>
               <SpeedDial.Button glassed icon="cloud-download" />
               <SpeedDial.Button glassed icon="copy" />
@@ -363,6 +422,13 @@ export default () => {
           }>
             <Button icon="dots" />
           </SpeedDial>
+        </Section>
+        <Section style={{ paddingTop: '10px', paddingBottom: '30px' }}>
+          <Breadcrumb>
+            <Breadcrumb.Item title={<div className="flex items-center"><Icon name="home" className="mr-2" /><span>Home</span></div>} />
+            <Breadcrumb.Item title={<div className="flex items-center"><Icon name="apps" className="mr-2" /><span>Application</span></div>} />
+            <Breadcrumb.Item title={<div className="flex items-center"><Icon name="send" className="mr-2" /><span>Button</span></div>} />
+          </Breadcrumb>
         </Section>
         <Section col className="--col --space-5" style={{ paddingTop: '30px', paddingBottom: '30px' }}>
           <Textbox placeholder="The placeholder" defaultValue="hoang.nguyen@notexsist.com" />
@@ -402,8 +468,73 @@ export default () => {
           <Textarea placeholder="The placeholder" defaultValue="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
           <InputNumber defaultValue="999" />
         </Section>
-        <Section className="--space-10" style={{ paddingTop: '30px', paddingBottom: '30px' }}>
-          
+        <Section className="--space-5" style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+          <div className="rc-overlay rc-tooltip --arrow --top" style={{ position: 'relative' }}>
+            Top Tooltip
+          </div>
+          <div className="rc-overlay rc-tooltip --arrow --right" style={{ position: 'relative' }}>
+            Right Tooltip
+          </div>
+          <div className="rc-overlay rc-tooltip --arrow --bottom" style={{ position: 'relative' }}>
+            Bottom Tooltip
+          </div>
+          <div className="rc-overlay rc-tooltip --arrow --left" style={{ position: 'relative' }}>
+            Left Tooltip
+          </div>
+        </Section>
+        <Section style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+          <div className="rc-overlay rc-popover --arrow --top" style={{ position: 'relative', flexGrow: 1 }}>
+            <Flex col style={{ padding: '0.5 1.5rem' }} className="w-full p-2">
+              <Flex items="center">
+                <Skeleton circle size="2rem" />
+                <Flex col className="ml-2 flex-1">
+                  <Skeleton p  w="70%" />
+                  <Skeleton p  w="20%" />
+                </Flex>
+              </Flex>
+              <Skeleton rect w="100%" h="100px" />
+            </Flex>
+            </div>
+        </Section>
+        <Section style={{ paddingTop: '30px', paddingBottom: '30px' }}>
+          <Dialog className="w-full">
+            <Dialog.Header>Dialog Title</Dialog.Header>
+            <Dialog.Body>
+              <Flex col style={{ padding: '0.5 1.5rem' }} className="w-full">
+                <Flex items="center">
+                  <Skeleton circle size="2rem" />
+                  <Flex col className="ml-2 flex-1">
+                    <Skeleton p  w="70%" />
+                    <Skeleton p  w="20%" />
+                  </Flex>
+                </Flex>
+                <Skeleton rect w="100%" h="100px" />
+              </Flex>
+            </Dialog.Body>
+            <Dialog.Footer className="flex justify-end">
+              <Button glassed><Icon name="apps" />Cancel</Button>
+              <Button glassed style={{ marginLeft: '0.5em' }}><Icon name="send" />OK</Button>
+            </Dialog.Footer>
+          </Dialog>
+        </Section>
+        <Section>
+          <PureNotification title="Notification title" className="w-full">
+            <Flex col style={{ padding: '0.5 1.5rem' }}>
+              <Flex items="center">
+                <Skeleton circle size="2rem" />
+                <Flex col className="ml-2 flex-1">
+                  <Skeleton p  w="70%" />
+                  <Skeleton p  w="20%" />
+                </Flex>
+              </Flex>
+            </Flex>
+          </PureNotification>
+        </Section>
+        <Section col className="--col --space-5">
+          <PureAlert info title="Info title" className="w-full" />
+          <PureAlert success title="Success title" className="w-full" />
+          <PureAlert error title="Error title" className="w-full" />
+          <PureAlert warning title="Warning title" className="w-full" />
         </Section>
       </Flex>
     </Flex>
