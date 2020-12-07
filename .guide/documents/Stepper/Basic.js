ee297718a8dc9753ff649f75c48dfe33d9495e3a
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-12-08 01:11:28 +0700

    updagte stepper

--- .guide/documents/Stepper/Basic.js
+++ .guide/documents/Stepper/Basic.js
@@ -1,32 +1,112 @@
 import React from 'react';
-import { Stepper, ButtonGroup, Button } from '@fork-ui/components/core';
+import {
+  Stepper,
+  ButtonGroup,
+  Button,
+  Flex,
+} from '@fork-ui/components/core';
 
 const Demo = () => {
-  const [props, stepper] = Stepper.useStepper({ maxStepIdx: 3 });
+  const [{
+    statuses,
+    goReset,
+    goBack,
+    goNext,
+    goSkip,
+    goCancel,
+    goFinish,
+  }] = Stepper.useStepper({
+    statuses: [
+      'completed',
+      'processing',
+    ],
+    activeStep: 1,
+    finishStep: 5,
+    skipable: [1, 2],
+    cancelable: [0, 2],
+  });
 
   return (
-    <div className="flex flex-col w-full">
-      <Stepper className="mb-10" {...props}>
-        <Stepper.Step title="Step One">
-          Description One
+    <Flex col span="full">
+      <Stepper className="mb-10">
+        <Stepper.Step status={statuses[0]}>
+          <Stepper.Button>1</Stepper.Button>
+          <Stepper.Content>
+            <Stepper.Title>
+              Step One
+            </Stepper.Title>
+            <Stepper.Description>
+              Description Step One
+            </Stepper.Description>
+          </Stepper.Content>
         </Stepper.Step>
-        <Stepper.Step title="Step Two">
-          Description Two
+        <Stepper.Step status={statuses[1]}>
+          <Stepper.Button>2</Stepper.Button>
+          <Stepper.Content>
+            <Stepper.Title>
+              Step Two
+            </Stepper.Title>
+            <Stepper.Description>
+              Description Step Two
+            </Stepper.Description>
+          </Stepper.Content>
         </Stepper.Step>
-        <Stepper.Step title="Step Three">
-          Description Three
+        <Stepper.Step status={statuses[2]}>
+          <Stepper.Button>3</Stepper.Button>
+          <Stepper.Content>
+            <Stepper.Title>
+              Step Three
+            </Stepper.Title>
+            <Stepper.Description>
+              Description Step Three
+            </Stepper.Description>
+          </Stepper.Content>
         </Stepper.Step>
-        <Stepper.Step title="Step Four">
-          Description Four
+        <Stepper.Step status={statuses[3]}>
+          <Stepper.Button>4</Stepper.Button>
+          <Stepper.Content>
+            <Stepper.Title>
+              Step Four
+            </Stepper.Title>
+            <Stepper.Description>
+              Description Step Four
+            </Stepper.Description>
+          </Stepper.Content>
+        </Stepper.Step>
+        <Stepper.Step status={statuses[4]}>
+          <Stepper.Button>5</Stepper.Button>
+          <Stepper.Content>
+            <Stepper.Title>
+              Step Five
+            </Stepper.Title>
+            <Stepper.Description>
+              Description Step Five
+            </Stepper.Description>
+          </Stepper.Content>
+        </Stepper.Step>
+        <Stepper.Step status={statuses[5]}>
+          <Stepper.Button>6</Stepper.Button>
+          <Stepper.Content>
+            <Stepper.Title>
+              Step Last
+            </Stepper.Title>
+            <Stepper.Description>
+              Description Step Last
+            </Stepper.Description>
+          </Stepper.Content>
         </Stepper.Step>
       </Stepper>
-      <ButtonGroup>
-        <Button onClick={stepper.handleReset}>Reset</Button>
-        <Button onClick={stepper.handleNext}>Next</Button>
-        <Button onClick={stepper.handleCancel}>Cancel</Button>
-        <Button onClick={stepper.handleFinish}>Finish</Button>
-      </ButtonGroup>
-    </div>
+      <Flex justify="center">
+        <ButtonGroup>
+          <Button onClick={goReset}>Reset</Button>
+          <Button onClick={goBack}>Back</Button>
+          <Button onClick={goNext}>Next</Button>
+          <Button onClick={goSkip}>Skip</Button>
+          <Button onClick={goCancel}>Cancel</Button>
+          <Button onClick={goFinish}>Finish</Button>
+        </ButtonGroup>
+      </Flex>
+    </Flex>
   );
 };
 
--- .guide/documents/Stepper/index.js
+++ .guide/documents/Stepper/index.js
@@ -2,30 +2,30 @@ import React from 'react';
 import Codebox from '../../Codebox';
 
 import Basic from './Basic';
-import Complex from './Complex';
-import Icon from './Icon';
-import Vertical from './Vertical';
+// import Complex from './Complex';
+// import Icon from './Icon';
+// import Vertical from './Vertical';
 
 export default () => {
   return (
     <div className="flex flex-col w-full">
       <div className="flex w-full" style={ { paddingBottom: '1px' } }>
-        <Codebox Component={ Complex } defaultShowCode />
+        <Codebox Component={ Basic } defaultShowCode />
       </div>
       <div className="flex w-full">
-        <div className="flex flex-col w-1/2">
-          <div className="w-full" style={ { paddingBottom: '1px' } }>
-            <Codebox Component={ Icon } />
-          </div>
-          <div className="w-full" style={ { paddingBottom: '1px' } }>
-            <Codebox Component={ Basic } />
-          </div>
+      {/* <div className="flex flex-col w-1/2">
+        <div className="w-full" style={ { paddingBottom: '1px' } }>
+          <Codebox Component={ Icon } />
         </div>
-        <div className="flex flex-col w-1/2" style={ { paddingLeft: '1px' } }>
-          <div className="w-full" style={ { paddingBottom: '1px' } }>
-            <Codebox Component={ Vertical } />
-          </div>
+        <div className="w-full" style={ { paddingBottom: '1px' } }>
+          <Codebox Component={ Basic } />
         </div>
+      </div>
+      <div className="flex flex-col w-1/2" style={ { paddingLeft: '1px' } }>
+        <div className="w-full" style={ { paddingBottom: '1px' } }>
+          <Codebox Component={ Vertical } />
+        </div>
+      </div> */}
       </div>
     </div>
   );
