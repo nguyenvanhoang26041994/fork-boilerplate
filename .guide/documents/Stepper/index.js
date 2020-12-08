8994bf0c3904ac7c6ba02b53875d1df7bff55608
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-12-09 00:20:48 +0700

    upgrate

--- .guide/documents/Stepper/index.js
+++ .guide/documents/Stepper/index.js
@@ -14,20 +14,23 @@ export default () => {
       <div className="flex w-full" style={ { paddingBottom: '1px' } }>
         <Codebox Component={ Basic } />
       </div>
+      <div className="flex w-full" style={ { paddingBottom: '1px' } }>
+        <Codebox Component={ Alternate } />
+      </div>
       <div className="flex w-full">
-      <div className="flex flex-col w-1/2">
-        <div className="w-full" style={ { paddingBottom: '1px' } }>
-          <Codebox Component={ Alternate } />
+        <div className="flex flex-col w-1/2">
+          {/* <div className="w-full" style={ { paddingBottom: '1px' } }>
+            <Codebox Component={ Alternate } />
+          </div> */}
+          {/* <div className="w-full" style={ { paddingBottom: '1px' } }>
+            <Codebox Component={ Basic } />
+          </div> */}
         </div>
-        {/* <div className="w-full" style={ { paddingBottom: '1px' } }>
-          <Codebox Component={ Basic } />
-        </div> */}
-      </div>
-      <div className="flex flex-col w-1/2" style={ { paddingLeft: '1px' } }>
-        <div className="w-full" style={ { paddingBottom: '1px' } }>
-          <Codebox Component={ Vertical } />
+        <div className="flex flex-col w-1/2" style={ { paddingLeft: '1px' } }>
+          <div className="w-full" style={ { paddingBottom: '1px' } }>
+            <Codebox Component={ Vertical } />
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
