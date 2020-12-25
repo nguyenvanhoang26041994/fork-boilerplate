7ed527ff93c8f52100f520fb6a6f4e24096e82fe
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-12-25 21:36:00 +0700

    dupaote avatar

--- .guide/documents/Radio/Controlled.js
+++ .guide/documents/Radio/Controlled.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Radio, RadioGroup } from '@fork-ui/components/core';
+import { Radio, useRadioGroup } from '@fork-ui/components/core';
 import Wrapper from '../../shared/Wrapper';
 
 
 const Demo = () => {
-  const [props] = RadioGroup.useRadioGroup('value-1');
-  // const [{ value, onChange }] = RadioGroup.useRadioGroup('value-1');
+  const [{ value, onChange }] = useRadioGroup('value-2');
 
   return (
     <Wrapper>
-      <RadioGroup {...props}>
-        <Radio value="value-1" />
-        <Radio value="value-2" />
-      </RadioGroup>
+      <Radio
+        value="value-1"
+        checked={'value-1' === value}
+        onChange={onChange}
+      />
+      <Radio
+        value="value-2"
+        checked={'value-2' === value}
+        onChange={onChange}
+      />
+      <Radio
+        value="value-3"
+        checked={'value-3' === value}
+        onChange={onChange}
+      />
     </Wrapper>
   );
 };
