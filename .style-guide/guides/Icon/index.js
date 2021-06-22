700e222ef39c37a73ac14f99f0f7b34901a8e3b4
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2021-06-22 23:43:55 +0700

    remove lazy icn from demo

--- .style-guide/guides/Icon/index.js
+++ .style-guide/guides/Icon/index.js
@@ -1,7 +1,7 @@
 import withGuides from '@style-guide/HOCs/withGuides';
 import Color from './Color';
 import Icon from './Icon';
-import LazyIcon from './LazyIcon';
+// import LazyIcon from './LazyIcon';
 import Size from './Size';
 import SomeNiceIcons from './SomeNiceIcons';
 // __INJECTED_LINE__
@@ -11,7 +11,7 @@ import * as allCode from './code';
 const demos = [
   { Component: Color, name: 'Color' },
   { Component: Icon, name: 'Icon' },
-  { Component: LazyIcon, name: 'LazyIcon' },
+  // { Component: LazyIcon, name: 'LazyIcon' },
   { Component: Size, name: 'Size' },
   { Component: SomeNiceIcons, name: 'SomeNiceIcons' },
   // __INJECTED_ITEM__
