0eed82d6742722424139cac59021b8443e8ecf3a
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2021-07-11 17:13:36 +0700

    Remove PureAlert

--- fork-guide/guides/Alert/HeaderAlert.js
+++ fork-guide/guides/Alert/HeaderAlert.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Wrapper } from '@fork-guide/components';
-import { PureAlert } from '@fork-ui/core';
-
-const A = styled.div`
-
-`;
-
 export default () => {
   return (
     <Wrapper>
-      <A>
-        
-      </A>
     </Wrapper>
   );
 };
--- fork-guide/guides/Alert/code.js
+++ fork-guide/guides/Alert/code.js
@@ -1,23 +1,11 @@
 export const Alert = {
   code: `import React from 'react';
 import { Wrapper } from '@fork-guide/components';
-import { PureAlert, Flex, Skeleton } from '@fork-ui/core';
+import { Flex, Skeleton } from '@fork-ui/core';
 
 export default () => {
   return (
     <Wrapper col span="20px">
-      <PureAlert type="info" className="w-full" style={{ boxShadow: 'var(--popover-box-shadow)' }}>
-        <PureAlert.Header>
-          INFO
-          <PureAlert.Closer />
-        </PureAlert.Header>
-        <PureAlert.Body className="pb-5">
-          <Flex col className="flex-1 fskele-animated" style={{ minWidth: '400px' }}>
-            <Skeleton shape="p" w="70%" />
-            <Skeleton shape="p" w="40%" />
-          </Flex>
-        </PureAlert.Body>
-      </PureAlert>
     </Wrapper>
   );
 };
@@ -29,18 +17,9 @@ export const HeaderAlert = {
   code: `import React from 'react';
 import styled from 'styled-components';
 import { Wrapper } from '@fork-guide/components';
-import { PureAlert } from '@fork-ui/core';
-
-const A = styled.div\`
-
-\`;
-
 export default () => {
   return (
     <Wrapper>
-      <A>
-        
-      </A>
     </Wrapper>
   );
 };
