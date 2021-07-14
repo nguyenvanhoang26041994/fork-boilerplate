46d687efe3f10553e819f982283e4e39f4a6453e
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2021-07-14 19:58:53 +0700

    Demo alert

--- fork-guide/guides/Alert/code.js
+++ fork-guide/guides/Alert/code.js
@@ -67,3 +67,63 @@ export default () => {
   demoName: 'Push Alert',
 }
 
+export const UpdateAlert = {
+  code: `import React from 'react';
+import { Wrapper } from '@fork-guide/components';
+import { Alert, Button, Loader } from '@fork-ui/core';
+import { Check } from '@fork-ui/icons/lazy';
+
+const ref = {
+  current: null,
+};
+const pushAlert = () => {
+  clearTimeout(ref.current);
+  Alert.ref.push(({ doClose }) => {
+    return (
+      <Alert
+        className="mx-2 my-1"
+        onClose={doClose}
+        closable={false}
+      >
+        We are still in progress
+        <span className="falert-closer" style={{ height: 32 }}>
+          <Loader.Spinner />
+        </span>
+      </Alert>
+    );
+  }, {
+    id: 'demo-id',
+  });
+
+  ref.current = setTimeout(() => {
+    Alert.ref.push(({ doClose }) => {
+      return (
+        <Alert
+          className="mx-2 my-1"
+          onClose={doClose}
+          closable={false}
+        >
+          All is done. Thank you!
+          <span className="falert-closer" style={{ height: 32 }}>
+            <Check color="var(--green-6)" />
+          </span>
+        </Alert>
+      );
+    }, {
+      id: 'demo-id',
+      autoClose: 2000,
+    });
+  }, 3000);
+};
+
+export default () => {
+  return (
+    <Wrapper>
+      <Button color="primary" onClick={() => pushAlert()}>Click Me</Button>
+    </Wrapper>
+  );
+};
+`,
+  demoName: 'Update Alert',
+}
+
--- fork-guide/guides/Alert/index.js
+++ fork-guide/guides/Alert/index.js
@@ -1,6 +1,7 @@
 import withGuides from '@fork-guide/HOCs/withGuides';
 import Alert from './Alert';
 import PushAlert from './PushAlert';
+import UpdateAlert from './UpdateAlert';
 // __INJECTED_LINE__
 
 import * as allCode from './code';
@@ -8,6 +9,7 @@ import * as allCode from './code';
 const demos = [
   { Component: Alert, name: 'Alert' },
   { Component: PushAlert, name: 'PushAlert' },
+  { Component: UpdateAlert, name: 'UpdateAlert' },
   // __INJECTED_ITEM__
 ];
 
